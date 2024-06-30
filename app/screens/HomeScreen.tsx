3572f03203319344f7e5e19e848940a0b3b0c834
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-06-30 17:23:25 +0530

    feat: Add Home screen to AppStackParamList and AppStack
    - Add "Home" screen to the AppStackParamList type definition in AppNavigator.tsx
    - Add "Home" screen to the AppStack component in AppNavigator.tsx
    - Create HomeScreen component in HomeScreen.tsx with basic structure and styling
    
    Related to: #123

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { FC } from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle } from "react-native"
+import { AppStackScreenProps } from "app/navigators"
+import { Screen, Text } from "app/components"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "app/models"
 
-// import { Container } from './styles';
+interface HomeScreenProps extends AppStackScreenProps<"Home"> {}
 
-export const screens: React.FC = () => {
-  return <View />;
+export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen() {
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  return (
+    <Screen style={$root} preset="scroll">
+      <Text text="home" />
+    </Screen>
+  )
+})
+
+
+const $root: ViewStyle = {
+  flex: 1,
 }
-
