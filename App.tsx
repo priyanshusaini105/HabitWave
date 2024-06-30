99037f6cbdc77e14cc8c7b4d820dce09ae4cd432
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-07-01 04:38:15 +0530

    feat: Update @ui-kitten/components and @ui-kitten/eva-icons versions
    This commit updates the versions of the "@ui-kitten/components" and "@ui-kitten/eva-icons" packages in the package.json and pnpm-lock.yaml files. The new versions are "@ui-kitten/components@5.3.1" and "@ui-kitten/eva-icons@5.3.1". This update ensures that the project is using the latest stable versions of these packages.
    
    Related to: #123

--- App.tsx
+++ App.tsx
@@ -2,11 +2,18 @@ import "@expo/metro-runtime"
 import React from "react"
 import * as SplashScreen from "expo-splash-screen"
 import App from "./app/app"
+import * as eva from "@eva-design/eva"
+import { ApplicationProvider } from "@ui-kitten/components"
+import { myTheme } from './custom-theme';
 
 SplashScreen.preventAutoHideAsync()
 
 function IgniteApp() {
-  return <App hideSplashScreen={SplashScreen.hideAsync} />
+  return (
+    <ApplicationProvider {...eva} theme={{...eva.light,...myTheme}}>
+      <App hideSplashScreen={SplashScreen.hideAsync} />
+    </ApplicationProvider>
+  )
 }
 
 export default IgniteApp
